8540dbc95cae226f0c15b11ec0fd6971ba049d4e
kimyoungjin <qazyj@naver.com>
2022-06-08 17:47:59 +0900

    [ADD] h2 database gradle, 설정

---
+++ src/test/java/api/productinformation/domain/UserTest.java
@@ -0,0 +1,24 @@
+package api.productinformation.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class UserTest {
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void userEntity() {
+
+    }
+}
