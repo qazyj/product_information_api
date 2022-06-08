5672aca42e5d0aaaa4ed6c60fcf28e299fb3c8a8
kimyoungjin <qazyj@naver.com>
2022-06-09 02:15:13 +0900

    [ADD] 임시 커밋

--- src/main/java/api/productinformation/service/UserService.java
+++ src/main/java/api/productinformation/service/UserService.java
@@ -1,6 +1,7 @@
 package api.productinformation.service;
 
 import api.productinformation.entity.user.User;
+import api.productinformation.entity.user.UserAdd;
 import api.productinformation.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,7 +14,8 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional
-    public void saveUser(User user){
+    public void saveUser(UserAdd userAdd){
+        User user = User.createUser(userAdd.getUsername(), userAdd.getUserType(), userAdd.getUserState());
         userRepository.save(user);
     }
 }
