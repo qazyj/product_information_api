b9e3431db37e4f49281313bedd5b98d6be999705
kimyoungjin <qazyj@naver.com>
2022-06-08 16:55:28 +0900

    [ADD] User entity 생성메서드, 비즈니스로직(회원탈퇴), protected 생성자

--- src/main/java/api/productinformation/domain/User.java
+++ src/main/java/api/productinformation/domain/User.java
@@ -1,11 +1,14 @@
 package api.productinformation.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
     @Id
     @GeneratedValue
@@ -24,14 +27,28 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserState userState;
 
-    public void setUserItem(UserItem userItem){
-        this.userItem = userItem;
+    //==생성 메서드==//
+    public static User createUser(String username, String userType, UserItem userItem){
+        User user = new User();
+        user.username = username;
+        if(userType.equals("일반"))
+            user.userType = Type.NORMAL;
+        else
+            user.userType = Type.CORPORATE;
+        user.userState = UserState.USE;
+        user.userItem = userItem;
+        return user;
     }
 
-    public User(String username, Type userType, UserState userState){
-        this.username = username;
-        this.userType = userType;
-        this.userState = userState;
-        this.userItem.addUsers(this);
+    //==비즈니스 로직==//
+    /**
+     * 회원 탈퇴
+     */
+    public void withdraw(){
+        if(this.userState == UserState.UNUSE){
+            throw new IllegalStateException("이미 탈퇴한 회원 입니다.");
+        }
+
+        this.userState = UserState.UNUSE;
     }
 }
