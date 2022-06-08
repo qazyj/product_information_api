257cce914516cdec031f371206a9318686c067ed
kimyoungjin <qazyj@naver.com>
2022-06-08 13:52:05 +0900

    [ADD] ItemPromotion Entity salePrice 컬럼  /  DateEntity 상속, 빈생성자, 파라미터 생성자

--- src/main/java/api/productinformation/domain/ItemPromotion.java
+++ src/main/java/api/productinformation/domain/ItemPromotion.java
@@ -1,6 +1,7 @@
 package api.productinformation.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -11,7 +12,8 @@ import javax.persistence.*;
  */
 @Entity
 @Getter
-public class ItemPromotion {
+@NoArgsConstructor
+public class ItemPromotion extends DateEntity {
     @Id
     @GeneratedValue
     @Column(name = "item_promotion_id")
@@ -24,4 +26,27 @@ public class ItemPromotion {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "promotion_id")
     private Promotion promotion;
+
+    private Long salePrice;
+
+    //==생성자로 값 주입==//
+    public ItemPromotion(Item item, Promotion promotion){
+        // Item 연관 관계 매핑
+        this.item = item;
+        item.getItemPromotions().add(this);
+        // Promotion 연관 관계 매핑
+        this.promotion = promotion;
+        promotion.getItemPromotions().add(this);
+
+        this.salePrice = setSalePrice(item, promotion);
+        setStartDate(promotion.getStartDate());
+        setEndDate(promotion.getEndDate());
+    }
+
+    public Long setSalePrice(Item item, Promotion promotion) {
+        if(promotion.getDiscountAmount() != null)
+            return item.getItemPrice() - promotion.getDiscountAmount();
+        else
+            return item.getItemPrice() - (long)(item.getItemPrice()*promotion.getDiscountRate());
+    }
 }
