31309180527577d7910eaaefdecb20e79aa96446
kimyoungjin <qazyj@naver.com>
2022-06-09 01:29:04 +0900

    [ADD] userservice

---
+++ src/main/java/api/productinformation/service/UserService.java
@@ -0,0 +1,19 @@
+package api.productinformation.service;
+
+import api.productinformation.entity.User;
+import api.productinformation.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+}
