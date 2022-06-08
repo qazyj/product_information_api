3bffbb090855630f889fbc0fb392a94b046b01ed
kimyoungjin <qazyj@naver.com>
2022-06-08 17:01:28 +0900

    [ADD] Promotion entity 생성 메서드

--- src/main/java/api/productinformation/domain/Promotion.java
+++ src/main/java/api/productinformation/domain/Promotion.java
@@ -1,12 +1,15 @@
 package api.productinformation.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Promotion extends DateEntity{
     @Id
     @GeneratedValue
@@ -21,4 +24,15 @@ public class Promotion extends DateEntity{
     private Integer discountAmount;
 
     private Double discountRate;
+
+    //==생성 메서드==//
+    public static Promotion createPromotion(String promotionName, Integer discountAmount, Double discountRate){
+        Promotion promotion = new Promotion();
+        promotion.promotionName = promotionName;
+        if(discountAmount == null)
+            promotion.discountRate = discountRate;
+        else
+            promotion.discountAmount = discountAmount;
+        return promotion;
+    }
 }
