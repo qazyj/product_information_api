b05a5d4dfd54de7159187ff893dfa82c34e88b7f
kimyoungjin <qazyj@naver.com>
2022-06-10 03:22:07 +0900

    ItemDto 변경에따른 테스트 수행

--- src/test/java/api/productinformation/service/ItemServiceTest.java
+++ src/test/java/api/productinformation/service/ItemServiceTest.java
@@ -1,5 +1,6 @@
 package api.productinformation.service;
 
+import api.productinformation.entity.Type;
 import api.productinformation.entity.item.Item;
 import api.productinformation.entity.item.ItemAdd;
 import api.productinformation.entity.item.ItemDto;
@@ -44,7 +45,10 @@ class ItemServiceTest {
         //then
         assertThat(itemDto.getId()).isEqualTo(findItem.getId());
         assertThat(itemDto.getItemName()).isEqualTo(findItem.getItemName());
-        assertThat(itemDto.getItemType()).isEqualTo(findItem.getItemType().toString());
+        if(findItem.getItemType().equals(Type.NORMAL))
+            assertThat(itemDto.getItemType()).isEqualTo("일반");
+        else
+            assertThat(itemDto.getItemType()).isEqualTo("기업회원상품");
         assertThat(itemDto.getItemPrice()).isEqualTo(findItem.getItemPrice());
         assertThat(itemDto.getStartDate()).isEqualTo(findItem.getStartDate());
         assertThat(itemDto.getEndDate()).isEqualTo(findItem.getEndDate());
