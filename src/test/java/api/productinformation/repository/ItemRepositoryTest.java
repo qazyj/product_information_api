8c594f3919af7da386beb73ba83070e614347875
kimyoungjin <qazyj@naver.com>
2022-06-10 16:59:27 +0900

    사용자가 구매할 ㅅㅜ있는 상품 정보를 반환하는 api 테스트 완료

--- src/test/java/api/productinformation/repository/ItemRepositoryTest.java
+++ src/test/java/api/productinformation/repository/ItemRepositoryTest.java
@@ -64,4 +64,41 @@ class ItemRepositoryTest {
         //then
         assertThat(results.size()).isEqualTo(103);
     }
+
+    @Test
+    public void 기업_아이템_추가_후_일반_유저_기업_유저회원이_살_수_있는_아이템_목록_수_테스트() throws Exception {
+        //given
+        // initDB에서 추가한 기본 데이터
+        for(int i = 0; i < 100; i++){
+            Item item = Item.createItem("item"+i, "기업회원상품", 1000L, "2021.1.1", "2022.12.24");
+            itemRepository.save(item);
+        }
+        em.flush();
+        em.clear();
+
+        //when
+        List<ItemDto> normalResults = itemRepository.findCanBuyItemListByType(Type.NORMAL);
+        List<ItemDto> corporateResults = itemRepository.findCanBuyItemList();
+
+        //then
+        assertThat(normalResults.size()).isEqualTo(2);
+        assertThat(corporateResults.size()).isEqualTo(103);
+    }
+
+    @Test
+    public void 살_수_없는_날짜_아이템_추가_후_테스트() throws Exception {
+        //given
+        for(int i = 0; i < 100; i++){
+            Item item = Item.createItem("item"+i, "기업회원상품", 1000L, "2021.1.1", "2022.1.1");
+            itemRepository.save(item);
+        }
+        em.flush();
+        em.clear();
+
+        //when
+        List<ItemDto> results = itemRepository.findCanBuyItemListByType(Type.NORMAL);
+
+        //then
+        assertThat(results.size()).isEqualTo(2);
+    }
 }
