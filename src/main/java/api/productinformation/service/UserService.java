39079a6be5a87e7897775f0308a86cd2df610ae6
kimyoungjin <qazyj@naver.com>
2022-06-11 02:29:29 +0900

    아이템프로모션 api UserService에 구현하여 ItemService로 코드 이동

--- src/main/java/api/productinformation/service/UserService.java
+++ src/main/java/api/productinformation/service/UserService.java
@@ -53,42 +53,4 @@ public class UserService {
         else
             return itemRepository.findCanBuyItemList();
     }
-
-    @Transactional(readOnly = true)
-    public ItemPromotionDto findItemPromotionById(Long id) {
-        Optional<Item> findItem = itemRepository.findByIdIncludeMinPromotion(id);
-        //if(findItem.isEmpty()) 널인경우 예외
-
-        sortSalePrice(findItem);
-
-        return getItemPromotionDto(findItem);
-    }
-
-    /**
-     * @return 정렬된 가격 순으로 아이템 날짜와 프로모션 날짜가 겹치면 dto 변환 후
-     * 단, 없을 경우 null 리턴
-     */
-    private ItemPromotionDto getItemPromotionDto(Optional<Item> findItem) {
-        for(ItemPromotion itemPromotion : findItem.get().getItemPromotions()){
-            if(itemPromotion.getSalePrice() <= 0L) continue;
-
-            if((findItem.get().getStartDate().isAfter(itemPromotion.getStartDate())
-                    && findItem.get().getStartDate().isBefore(itemPromotion.getEndDate())) ||
-                    (findItem.get().getEndDate().isAfter(itemPromotion.getStartDate())
-                            && findItem.get().getEndDate().isBefore(itemPromotion.getEndDate()))) {
-                return new ItemPromotionDto(findItem.get(), itemPromotion.getPromotion());
-            }
-        }
-        // 없는 경우 프로모션 없으면 예외
-        return null;
-    }
-
-    private void sortSalePrice(Optional<Item> findItem) {
-        Collections.sort(findItem.get().getItemPromotions(), new Comparator<ItemPromotion>() {
-            @Override
-            public int compare(ItemPromotion o1, ItemPromotion o2) {
-                return o1.getSalePrice().intValue() - o2.getSalePrice().intValue();
-            }
-        });
-    }
 }
